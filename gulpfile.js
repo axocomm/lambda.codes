875faa4012ec8aed1819e391e04d921d2fca6523
axocomm <axocomm@gmail.com>
2016-04-17 10:39:20 -0400

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var del = require('del');
+
+var manifest = {
+  scss: ['resources/src/scss/styles.scss']
+};
+
+var dest = {
+  css: 'resources/public/css'
+};
+
+gulp.task('clean', function () {
+  return del([dest.css]);
+});
+
+gulp.task('scss', function () {
+  return gulp.src(manifest.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(dest.css));
+});
+
+gulp.task('build', ['scss']);
+
+gulp.task('default', ['build']);
+
+gulp.task('d', ['default'], function () {
+  return gulp.watch(manifest.scss, ['default']);
+});
